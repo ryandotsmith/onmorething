37cb831338adbc207e10a43a66c6d835c04e3a56
Ryan Smith <this.ryansmith@gmail.com>
2009-01-14 14:44:01 -0600

    froze rails and added some style to the error messages

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,14 @@
 class Idea < ActiveRecord::Base
-  validates_presence_of :jist, :message => "are you some sort of nihilist? "
+  validates_presence_of :jist, 
+    :message => " it seems that you do not have much of an idea."
   validates_length_of   :jist,:maximum => 144, :message => "please keep your idea compact & concise "
+
+  def self.human_attribute_name(attribute_key_name)
+        if attribute_key_name.to_sym == :jist
+          " "
+        else
+          super
+        end
+      end
+
 end
