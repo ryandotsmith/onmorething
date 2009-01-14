9b324e70bfda0bd8feb0ed04a155f2869375c9cf
Ryan Smith <this.ryansmith@gmail.com>
2009-01-13 23:49:38 -0600

    added some simple validations

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,4 @@
 class Idea < ActiveRecord::Base
+  validates_presence_of :jist, :message => "are you some sort of nihilist? "
+  validates_length_of   :jist,:maximum => 144, :message => "please keep your idea compact & concise "
 end
